1008a6b34f56869464e230142ba607f6375f3bf6
chanmin0905 <jcm0905@gmail.com>
2022-06-18 19:52:16 +0900

    [feature] : 04-08-assignment-solution

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
+
 import Card from "../UI/Card";
 
 import "./Expenses.css";
 
 const Expenses = ({items}) => {
+  const [filteredYear, setFilteredYear] = useState("2022");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+    console.log(filteredYear);
+  };
+
   return (
     <Card className="expenses">
+      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
       {items.map((expense) => {
         return (
           <ExpenseItem
