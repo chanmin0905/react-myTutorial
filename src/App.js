6a09587900bd276b6d0de4bc17e9f765c6e7df81
chanmin0905 <jcm0905@gmail.com>
2022-06-18 11:49:43 +0900

    [feature] : 11-finished

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
 
 function App() {
   const expenses = [
@@ -22,6 +22,13 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+
+  // return React.createElement(
+  //   "div",
+  //   {},
+  //   React.createElement("h2", {}, "Let's get started!"),
+  //   React.createElement(Expenses, {items: expenses})
+  // );
 
   return (
     <div>
